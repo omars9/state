18d6a78a5178bc0cfcbb1030843d9838bf931ea0
omars9 <saidaomarzico@gmail.com>
2022-03-12 17:23:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import "./App.css";
+import image from "./img/logo.svg"
+export default class App extends Component {
+  state ={
+    fullName:"Omar",
+    bio:"test",
+    profession:"test",
+    shows:false,
+    image : <img src={image} alt='photo'></img>
 }
 
-export default App;
+handleShow = () => {
+     this.setState({shows:!this.state.shows})
+    }
+  render() {
+    return (
+      <div  className='App'>
+       <button  onClick={this.handleShow} >   {this.state.shows ? "Hide" : "Show"}  </button>    
+      {this.state.shows ?(
+        
+        
+        <div>
+          {this.state.image}
+          <h1> {this.state.fullName} </h1>
+          <h1> {this.state.bio} </h1>
+          <h1> {this.state.profession} </h1>
+        </div>
+      ): null}
+
+      
+      </div>
+    )
+  }
+}
